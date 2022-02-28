64b1588bfa6c22a0b70c2da79766ddc5e1321604
K3rn3l_P4n1c <53450448+Kazuto-Kawahara@users.noreply.github.com>
2022-02-28 14:47:06 +0300

    SortedWithFolders

---
+++ Codes/Lec.10 FunctionsWithParameters.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+
+void age (int x) { // Here with the parameters you can define the type of the variable it needs to work. For example our function here needs a number 
+	cout<<"I am "<<x<<" years old"; 
+}
+int main () {
+	age(22); //As you can see with this function when calling the function you can give the variable in the paranthesis
+	return 0;
+}
