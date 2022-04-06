56f4be245471c099dcd635e35e04d085b7fe608f
K3rn3l_P4n1c <53450448+Kazuto-Kawahara@users.noreply.github.com>
2022-04-06 17:46:58 +0300

    Hackkerrank 1-4 Added

---
+++ C++_Hackerrank/Basic Data Types.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int x;
+    long y;
+    char c;
+    float z;
+    double b;
+    
+    cin>>x>>y>>c>>z>>b;
+    
+    cout<<x<<endl;
+    cout<<y<<endl;
+    cout<<c<<endl;
+    cout<<setprecision(15)<<z<<endl;
+    cout<<b<<endl;
+    
+    
+    
+    return 0;
+}
